8f67544ea9396e2e60c91655386761af22e487e4
suhag-alamin <developersuhag@gmail.com>
2023-07-17 12:08:35 +0600

    husky and lint-staged fixed

--- src/server.ts
+++
@@ -1,9 +0,0 @@
-const favoriteFruits: string[] = ['apple', 'strawberry', 'orange'];
-
-function addFruit(fruit: string) {
-  const test = 'dd';
-  const test2 = 'dd';
-  const test3 = 'dd';
-  const test4 = 'dd';
-  favoriteFruits.push(fruit);
-}
