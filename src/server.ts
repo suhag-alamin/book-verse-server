bcc8592b4e0ea26ccdf3ccdfe42a5c7f4cc5e26b
suhag-alamin <developersuhag@gmail.com>
2023-07-17 11:55:48 +0600

    linting setup

--- src/server.ts
+++ src/server.ts
@@ -3,5 +3,6 @@ const favoriteFruits: string[] = ['apple', 'strawberry', 'orange'];
 function addFruit(fruit: string) {
   const test = 'dd';
   const test2 = 'dd';
+  const test3 = 'dd';
   favoriteFruits.push(fruit);
 }
