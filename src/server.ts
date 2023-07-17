53482397ae1c1406914e19ca250a720b5e99a409
suhag-alamin <developersuhag@gmail.com>
2023-07-17 15:17:25 +0600

    uncaught exception, unhandled rejection and sigterm handle

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,42 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import mongoose from 'mongoose';
 import config from './config';
-// import { Server } from 'http';
+import { Server } from 'http';
 import app from './app';
 
-// eslint-disable-next-line no-unused-vars
-// let server: Server;
+process.on('uncaughtException', error => {
+  console.log(error);
+  process.exit(1);
+});
+
+let server: Server;
 
 const main = async () => {
   try {
     await mongoose.connect(config.database_url as string);
     console.log('Database connected');
 
-    app.listen(config.port, () => {
+    server = app.listen(config.port, () => {
       console.log(`BookVerse app listening on port ${config.port}`);
     });
   } catch (error) {
     console.log(error);
   }
+
+  process.on('unhandledRejection', error => {
+    if (server) {
+      console.log(error);
+      process.exit(1);
+    } else {
+      process.exit(1);
+    }
+  });
 };
 
 main();
+
+process.on('SIGTERM', () => {
+  console.log('SIGTERM is recived');
+  if (server) {
+    server.close();
+  }
+});
