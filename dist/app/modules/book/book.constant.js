1f9303845211ce61135eff932456e95b617ac83b
suhag-alamin <developersuhag@gmail.com>
2023-07-20 15:56:16 +0600

    making production ready

--- dist/app/modules/book/book.constant.js
+++ dist/app/modules/book/book.constant.js
@@ -1,5 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.bookFilterableFields = exports.bookSearchableFields = void 0;
-exports.bookSearchableFields = ['title', 'author', 'genre'];
+exports.bookSearchableFields = [
+    'title',
+    'author.firstName',
+    'author.lastName',
+    'genre',
+];
 exports.bookFilterableFields = ['searchTerm', 'genre', 'publicationYear'];
--- dist/app/modules/book/book.controller.js
+++ dist/app/modules/book/book.controller.js
@@ -14,11 +14,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.BookController = void 0;
 const http_status_1 = __importDefault(require("http-status"));
+const pagination_1 = require("../../../constants/pagination");
 const catchAsync_1 = __importDefault(require("../../../shared/catchAsync"));
-const sendResponse_1 = __importDefault(require("../../../shared/sendResponse"));
-const book_service_1 = require("./book.service");
 const pick_1 = __importDefault(require("../../../shared/pick"));
+const sendResponse_1 = __importDefault(require("../../../shared/sendResponse"));
 const book_constant_1 = require("./book.constant");
+const book_service_1 = require("./book.service");
 const createBookController = (0, catchAsync_1.default)((req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const book = req.body;
     const result = yield book_service_1.BookService.createBook(book);
@@ -31,7 +32,7 @@ const createBookController = (0, catchAsync_1.default)((req, res) => __awaiter(v
 }));
 const getAllBooksController = (0, catchAsync_1.default)((req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const filters = (0, pick_1.default)(req.query, book_constant_1.bookFilterableFields);
-    const paginationOptions = (0, pick_1.default)(req.query, book_constant_1.bookSearchableFields);
+    const paginationOptions = (0, pick_1.default)(req.query, pagination_1.paginationFields);
     const result = yield book_service_1.BookService.getAllBooks(filters, paginationOptions);
     (0, sendResponse_1.default)(res, {
         statusCode: http_status_1.default.OK,
