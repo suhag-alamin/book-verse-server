dcd617e7c3a505b6f99c8b020919f29bf29c8293
suhag-alamin <developersuhag@gmail.com>
2023-07-25 00:14:11 +0600

    update refresh token method

--- dist/app/middleware/globalErrorHandler.js
+++ dist/app/middleware/globalErrorHandler.js
@@ -1,79 +1,92 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const http_status_1 = __importDefault(require("http-status"));
-const mongodb_1 = require("mongodb");
-const zod_1 = require("zod");
-const config_1 = __importDefault(require("../../config"));
-const ApiError_1 = __importDefault(require("../../errors/ApiError"));
-const handleCastError_1 = __importDefault(require("../../errors/handleCastError"));
-const handleValidationError_1 = __importDefault(require("../../errors/handleValidationError"));
-const handleZodError_1 = __importDefault(require("../../errors/handleZodError"));
+'use strict';
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
+Object.defineProperty(exports, '__esModule', { value: true });
+const http_status_1 = __importDefault(require('http-status'));
+const mongodb_1 = require('mongodb');
+const zod_1 = require('zod');
+const config_1 = __importDefault(require('../../config'));
+const ApiError_1 = __importDefault(require('../../errors/ApiError'));
+const handleCastError_1 = __importDefault(
+  require('../../errors/handleCastError'),
+);
+const handleValidationError_1 = __importDefault(
+  require('../../errors/handleValidationError'),
+);
+const handleZodError_1 = __importDefault(
+  require('../../errors/handleZodError'),
+);
 const globalErrorHandler = (error, req, res, next) => {
-    let statusCode = 500;
-    let message = 'Something went wrong';
-    let errorMessages = [];
-    // console.log('error', error);
-    if ((error === null || error === void 0 ? void 0 : error.name) === 'ValidationError') {
-        const simplifiedError = (0, handleValidationError_1.default)(error);
-        statusCode = simplifiedError.statusCode;
-        message = simplifiedError.message;
-        errorMessages = simplifiedError.errorMessages;
-    }
-    else if (error.name === 'CastError') {
-        const simplifiedError = (0, handleCastError_1.default)(error);
-        statusCode = simplifiedError.statusCode;
-        message = simplifiedError.message;
-        errorMessages = simplifiedError.errorMessages;
-    }
-    else if (error instanceof zod_1.ZodError) {
-        const simplifiedError = (0, handleZodError_1.default)(error);
-        statusCode = simplifiedError.statusCode;
-        message = simplifiedError.message;
-        errorMessages = simplifiedError.errorMessages;
-    }
-    else if (error instanceof ApiError_1.default) {
-        statusCode = error.statusCode;
-        message = error.message;
-        errorMessages = error.message
-            ? [
-                {
-                    path: '',
-                    message: error.message,
-                },
-            ]
-            : [];
-    }
-    else if (error instanceof mongodb_1.MongoServerError && error.code === 11000) {
-        statusCode = http_status_1.default.CONFLICT;
-        message =
-            'Already have an account with this email. Please login or use another email';
-        const { keyValue } = error;
-        errorMessages = [
-            {
-                path: Object.keys(keyValue).join(','),
-                message: `Duplicate entry for ${Object.keys(keyValue).join(',')}: ${Object.values(keyValue).join(',')}`,
-            },
-        ];
-    }
-    else if (error instanceof Error) {
-        message = error.message;
-        errorMessages = (error === null || error === void 0 ? void 0 : error.message)
-            ? [
-                {
-                    path: '',
-                    message: error.message,
-                },
-            ]
-            : [];
-    }
-    res.status(statusCode).json({
-        success: false,
-        message,
-        errorMessages,
-        stack: config_1.default.env !== 'production' ? error.stack : undefined,
-    });
+  let statusCode = 500;
+  let message = 'Something went wrong';
+  let errorMessages = [];
+  // console.log('error', error);
+  if (
+    (error === null || error === void 0 ? void 0 : error.name) ===
+    'ValidationError'
+  ) {
+    const simplifiedError = (0, handleValidationError_1.default)(error);
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+    errorMessages = simplifiedError.errorMessages;
+  } else if (error.name === 'CastError') {
+    const simplifiedError = (0, handleCastError_1.default)(error);
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+    errorMessages = simplifiedError.errorMessages;
+  } else if (error instanceof zod_1.ZodError) {
+    const simplifiedError = (0, handleZodError_1.default)(error);
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+    errorMessages = simplifiedError.errorMessages;
+  } else if (error instanceof ApiError_1.default) {
+    statusCode = error.statusCode;
+    message = error.message;
+    errorMessages = error.message
+      ? [
+          {
+            path: '',
+            message: error.message,
+          },
+        ]
+      : [];
+  } else if (
+    error instanceof mongodb_1.MongoServerError &&
+    error.code === 11000
+  ) {
+    statusCode = http_status_1.default.CONFLICT;
+    message =
+      'Already have an account with this email. Please login or use another email';
+    const { keyValue } = error;
+    errorMessages = [
+      {
+        path: Object.keys(keyValue).join(','),
+        message: `Duplicate entry for ${Object.keys(keyValue).join(
+          ',',
+        )}: ${Object.values(keyValue).join(',')}`,
+      },
+    ];
+  } else if (error instanceof Error) {
+    message = error.message;
+    errorMessages = (
+      error === null || error === void 0 ? void 0 : error.message
+    )
+      ? [
+          {
+            path: '',
+            message: error.message,
+          },
+        ]
+      : [];
+  }
+  res.status(statusCode).json({
+    success: false,
+    message,
+    errorMessages,
+    stack: config_1.default.env !== 'production' ? error.stack : undefined,
+  });
 };
 exports.default = globalErrorHandler;
--- dist/app/modules/review/review.service.js
+++ dist/app/modules/review/review.service.js
@@ -1,40 +1,76 @@
-"use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+'use strict';
+var __awaiter =
+  (this && this.__awaiter) ||
+  function (thisArg, _arguments, P, generator) {
+    function adopt(value) {
+      return value instanceof P
+        ? value
+        : new P(function (resolve) {
+            resolve(value);
+          });
+    }
     return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
+      function fulfilled(value) {
+        try {
+          step(generator.next(value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function rejected(value) {
+        try {
+          step(generator['throw'](value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function step(result) {
+        result.done
+          ? resolve(result.value)
+          : adopt(result.value).then(fulfilled, rejected);
+      }
+      step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
+  };
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
+Object.defineProperty(exports, '__esModule', { value: true });
 exports.ReviewService = void 0;
-const http_status_1 = __importDefault(require("http-status"));
-const ApiError_1 = __importDefault(require("../../../errors/ApiError"));
-const book_model_1 = require("../book/book.model");
-const review_model_1 = require("./review.model");
-const addReview = (payload) => __awaiter(void 0, void 0, void 0, function* () {
+const http_status_1 = __importDefault(require('http-status'));
+const ApiError_1 = __importDefault(require('../../../errors/ApiError'));
+const book_model_1 = require('../book/book.model');
+const review_model_1 = require('./review.model');
+const addReview = payload =>
+  __awaiter(void 0, void 0, void 0, function* () {
     // check if the book exist or not
     const isBookExist = yield book_model_1.Book.findById(payload.book);
     if (!isBookExist) {
-        throw new ApiError_1.default(http_status_1.default.NOT_FOUND, 'Book does not exist!');
+      throw new ApiError_1.default(
+        http_status_1.default.NOT_FOUND,
+        'Book does not exist!',
+      );
     }
-    const result = (yield review_model_1.Review.create(payload)).populate('book');
+    const result = (yield review_model_1.Review.create(payload)).populate(
+      'book',
+    );
     return result;
-});
-const getAllReviews = (bookId) => __awaiter(void 0, void 0, void 0, function* () {
-    const result = yield review_model_1.Review.find({
+  });
+const getAllReviews = bookId =>
+  __awaiter(void 0, void 0, void 0, function* () {
+    const result = yield review_model_1.Review.find(
+      {
         book: bookId,
-    }, {
+      },
+      {
         review: 1,
-    });
+      },
+    );
     return result;
-});
+  });
 exports.ReviewService = {
-    addReview,
-    getAllReviews,
+  addReview,
+  getAllReviews,
 };
