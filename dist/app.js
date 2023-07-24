6a4f68a3f5294a2207a3cdb1efcb651f99e7e359
suhag-alamin <developersuhag@gmail.com>
2023-07-24 14:30:55 +0600

    handle duplicate email error

--- dist/app.js
+++ dist/app.js
@@ -1,37 +1,43 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
+'use strict';
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
+Object.defineProperty(exports, '__esModule', { value: true });
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable no-unused-vars */
-const cookie_parser_1 = __importDefault(require("cookie-parser"));
-const cors_1 = __importDefault(require("cors"));
-const express_1 = __importDefault(require("express"));
-const http_status_1 = __importDefault(require("http-status"));
-const globalErrorHandler_1 = __importDefault(require("./app/middleware/globalErrorHandler"));
-const routes_1 = __importDefault(require("./app/routes"));
+const cookie_parser_1 = __importDefault(require('cookie-parser'));
+const cors_1 = __importDefault(require('cors'));
+const express_1 = __importDefault(require('express'));
+const http_status_1 = __importDefault(require('http-status'));
+const globalErrorHandler_1 = __importDefault(
+  require('./app/middleware/globalErrorHandler'),
+);
+const routes_1 = __importDefault(require('./app/routes'));
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
 // parser
 app.use(express_1.default.json());
 app.use((0, cookie_parser_1.default)());
-app.use(express_1.default.urlencoded({
+app.use(
+  express_1.default.urlencoded({
     extended: true,
-}));
+  }),
+);
 app.use('/api/v1', routes_1.default);
 app.use(globalErrorHandler_1.default);
 // handle not found
 app.use((req, res, next) => {
-    res.status(http_status_1.default.NOT_FOUND).json({
-        success: false,
-        message: 'Not Found',
-        errorMessages: [
-            {
-                path: req.originalUrl,
-                message: 'API end point not found',
-            },
-        ],
-    });
+  res.status(http_status_1.default.NOT_FOUND).json({
+    success: false,
+    message: 'Not Found',
+    errorMessages: [
+      {
+        path: req.originalUrl,
+        message: 'API end point not found',
+      },
+    ],
+  });
 });
 exports.default = app;
--- dist/app/modules/auth/auth.model.js
+++ dist/app/modules/auth/auth.model.js
@@ -1,76 +1,111 @@
-"use strict";
+'use strict';
 /* eslint-disable @typescript-eslint/no-this-alias */
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+var __awaiter =
+  (this && this.__awaiter) ||
+  function (thisArg, _arguments, P, generator) {
+    function adopt(value) {
+      return value instanceof P
+        ? value
+        : new P(function (resolve) {
+            resolve(value);
+          });
+    }
     return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
+      function fulfilled(value) {
+        try {
+          step(generator.next(value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function rejected(value) {
+        try {
+          step(generator['throw'](value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function step(result) {
+        result.done
+          ? resolve(result.value)
+          : adopt(result.value).then(fulfilled, rejected);
+      }
+      step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
+  };
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
+Object.defineProperty(exports, '__esModule', { value: true });
 exports.User = void 0;
-const mongoose_1 = require("mongoose");
-const bcrypt_1 = __importDefault(require("bcrypt"));
-const config_1 = __importDefault(require("../../../config"));
-const auth_constant_1 = require("./auth.constant");
-const userSchema = new mongoose_1.Schema({
+const mongoose_1 = require('mongoose');
+const bcrypt_1 = __importDefault(require('bcrypt'));
+const config_1 = __importDefault(require('../../../config'));
+const auth_constant_1 = require('./auth.constant');
+const userSchema = new mongoose_1.Schema(
+  {
     name: {
-        type: {
-            firstName: {
-                type: String,
-                required: true,
-            },
-            lastName: {
-                type: String,
-                required: true,
-            },
+      type: {
+        firstName: {
+          type: String,
+          required: true,
         },
-        required: true,
+        lastName: {
+          type: String,
+          required: true,
+        },
+      },
+      required: true,
     },
     email: {
-        type: String,
-        required: true,
-        unique: true,
+      type: String,
+      required: true,
+      unique: true,
     },
     password: {
-        type: String,
-        required: true,
-        select: 0,
+      type: String,
+      required: true,
+      select: 0,
     },
     role: {
-        type: String,
-        enum: auth_constant_1.role,
-        default: 'user',
+      type: String,
+      enum: auth_constant_1.role,
+      default: 'user',
     },
-}, {
+  },
+  {
     timestamps: true,
-});
+  },
+);
 userSchema.methods.isUserExist = function (email) {
-    return __awaiter(this, void 0, void 0, function* () {
-        const user = yield exports.User.findOne({ email }, {
-            _id: 1,
-            email: 1,
-            password: 1,
-            role: 1,
-        }).lean();
-        return user;
-    });
+  return __awaiter(this, void 0, void 0, function* () {
+    const user = yield exports.User.findOne(
+      { email },
+      {
+        _id: 1,
+        email: 1,
+        password: 1,
+        role: 1,
+      },
+    ).lean();
+    return user;
+  });
 };
 userSchema.methods.isPasswordMatch = function (givenPassword, savedPassword) {
-    return __awaiter(this, void 0, void 0, function* () {
-        return yield bcrypt_1.default.compare(givenPassword, savedPassword);
-    });
+  return __awaiter(this, void 0, void 0, function* () {
+    return yield bcrypt_1.default.compare(givenPassword, savedPassword);
+  });
 };
 userSchema.pre('save', function (next) {
-    return __awaiter(this, void 0, void 0, function* () {
-        const user = this;
-        user.password = yield bcrypt_1.default.hash(user.password, Number(config_1.default.bcrypt_slat_rounds));
-        next();
-    });
+  return __awaiter(this, void 0, void 0, function* () {
+    const user = this;
+    user.password = yield bcrypt_1.default.hash(
+      user.password,
+      Number(config_1.default.bcrypt_slat_rounds),
+    );
+    next();
+  });
 });
 exports.User = (0, mongoose_1.model)('User', userSchema);
